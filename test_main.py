e16af70af03e240b2b767a40476609b7038118a1
Leibniz137 <nate@thenateway.io>
2021-03-03 17:18:29 -0800

    also test 3 of 5

--- test_main.py
+++ test_main.py
@@ -1,17 +1,27 @@
+from itertools import permutations
+
 import pytest
 
 from main import SeedPhraseToSeedPhraseSecretSharer
 
 
-@pytest.mark.parametrize('i, j', [
-    (0, 1),
-    (0, 2),
-    (1, 2),
-])
+@pytest.mark.parametrize('i, j', permutations(range(0, 2), r=2))
 def test_reconstruction(i, j):
     m = SeedPhraseToSeedPhraseSecretSharer.mnemo.generate().split(' ')
     shares = SeedPhraseToSeedPhraseSecretSharer.split_secret(m, 2, 3)
     share_i = shares[i]
     share_j = shares[j]
     m2 = SeedPhraseToSeedPhraseSecretSharer.recover_secret([share_i, share_j])
+    assert m == m2
+
+
+@pytest.mark.parametrize('i, j, k', permutations(range(0, 5), r=3))
+def test_reconstruction_3_of_5(i, j, k):
+    m = SeedPhraseToSeedPhraseSecretSharer.mnemo.generate().split(' ')
+    shares = SeedPhraseToSeedPhraseSecretSharer.split_secret(m, 3, 5)
+    m2 = SeedPhraseToSeedPhraseSecretSharer.recover_secret([
+        shares[i],
+        shares[j],
+        shares[k]
+    ])
     assert m == m2
