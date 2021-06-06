0dbd7c1b9147fc3a67a6e66df155510966470570
Leibniz137 <nate@thenateway.io>
2021-06-06 13:27:52 -0700

    add testcases

--- test_main.py
+++ test_main.py
@@ -25,3 +25,25 @@ def test_reconstruction_3_of_5(i, j, k):
         shares[k]
     ])
     assert m == m2
+
+
+@pytest.mark.parametrize('i, j', permutations(range(0, 2), r=2))
+def test_reconstruction_256(i, j):
+    m = SeedPhraseToSeedPhraseSecretSharer.mnemo.generate(strength=256).split(' ')   # noqa: E501
+    shares = SeedPhraseToSeedPhraseSecretSharer.split_secret(m, 2, 3)
+    share_i = shares[i]
+    share_j = shares[j]
+    m2 = SeedPhraseToSeedPhraseSecretSharer.recover_secret([share_i, share_j])
+    assert m == m2
+
+
+@pytest.mark.parametrize('i, j, k', permutations(range(0, 5), r=3))
+def test_reconstruction_3_of_5_256(i, j, k):
+    m = SeedPhraseToSeedPhraseSecretSharer.mnemo.generate(strength=256).split(' ')   # noqa: E501
+    shares = SeedPhraseToSeedPhraseSecretSharer.split_secret(m, 3, 5)
+    m2 = SeedPhraseToSeedPhraseSecretSharer.recover_secret([
+        shares[i],
+        shares[j],
+        shares[k]
+    ])
+    assert m == m2
